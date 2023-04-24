5912b25294759ce5a603b2ca9ba48296c6b97da9
Vishal Singh <vishalkumar70854@gmail.com>
2023-04-24 17:18:51 +0530

    First Commit!

---
+++ schemas.py
@@ -0,0 +1,44 @@
+from marshmallow import Schema, fields
+
+class PlainItemSchema(Schema):
+    id = fields.Int(dump_only=True) # used for sending data back to the client
+    name = fields.Str(required=True)    # required field
+    price = fields.Float(required=True)
+
+class PlainStoreSchema(Schema):
+    id = fields.Int(dump_only=True) #dump_only=True -> used for sending data back to the client only
+    name = fields.Str(required=True)    # required field
+   
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+    
+class ItemUpdateSchema(Schema):
+    name = fields.Str() # optional
+    price = fields.Float()  # optional
+    store_id = fields.Int() # optional
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+    
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema(), dump_only=True))
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+    
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
+    
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)    # it will not send data back to client as password is sensitive
